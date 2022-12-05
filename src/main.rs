6b454c4277a526d3d81f8c8199e244ba4728523d
DMD0om <totallyradomperson@gmail.com>
2022-12-05 09:47:04 +0100

    feat: make it work

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,44 @@ use std::io::{self, BufRead};
 use std::fs::File;
 use std::collections::HashSet;
 
-fn main() {
-    loop {
-        println!("Loading dictionary...");
+struct Assembler {
+    word_bank: HashSet<String>
+}
 
-        // Open file
+impl Assembler {
+    fn build_assembler() -> Assembler {
+        Assembler {
+            word_bank: Self::load_dictionary()
+        }
+    }
+    
+    // Load word dictionary to memory
+    fn load_dictionary() -> HashSet<String> {
+        println!("Opening file...");
+
         let file: File = match File::open("slowa.txt") {
             Result::Ok(num) => num,
             Result::Err(_) => panic!("File not found.")
         };
+    
+        println!("Loading dictionary...");
 
-        // Init reader
         let buf_reader = io::BufReader::new(file);
 
-        // Init map and hash words in the file
         let mut word_bank: HashSet<String> = HashSet::new();
         for line in buf_reader.lines() {
             word_bank.insert(line.unwrap());
         }
-
+        
         println!("Dictionary loaded.");
+        return word_bank;
+    }
+}
 
+fn main() {
+    let assembler: Assembler = Assembler::build_assembler();
+
+    loop {
         // Read user input
         println!("Input letters: ");
         let mut letters: String = String::new();
@@ -31,18 +48,26 @@ fn main() {
             .read_line(&mut letters)
             .expect("Unexpected error");
 
+        // Remove trailing formatting that windows appends to strings
+        letters.truncate(letters.chars().count() - 2);
+
+        if letters.chars().count() > 10 {
+            println!("Too many letters, try again");
+            continue;
+        }
+
         // Assemble words and print
-        let result = assemble(letters, word_bank);
+        let result = find_words(letters, &assembler.word_bank);
         println!("{:?}", result);
     }
 }
 
 // Assemble and return all possible words
-fn assemble(letters: String, word_bank: HashSet<String>) -> HashSet<String> {
+fn find_words(letters: String, word_bank: &HashSet<String>) -> HashSet<String> {
     // Keeping track of total words found, used letters, and currently assembled word
     let mut result = HashSet::<String>::new();
-    let mut used = Vec::<bool>::with_capacity(letters.chars().count());
     let mut chars: Vec<char> = letters.chars().collect();
+    let mut used = vec![false; letters.chars().count()];
     let mut word = String::new();
 
     // Recursion
@@ -51,7 +76,6 @@ fn assemble(letters: String, word_bank: HashSet<String>) -> HashSet<String> {
     return result;
 }
 
-// Depth-for-search
 fn dfs(word: &mut String, letters: &mut Vec<char>, used: &mut Vec<bool>, result: &mut HashSet<String>, word_bank: &HashSet<String>) {
     if word_bank.contains(word) {
         result.insert(word.clone());
@@ -61,13 +85,13 @@ fn dfs(word: &mut String, letters: &mut Vec<char>, used: &mut Vec<bool>, result:
         return;
     }
 
-    for (i, letter) in letters.iter().enumerate() {
+    for (i, letter) in letters.clone().iter().enumerate() {
         if !used[i] {
             word.push(letter.clone());
             used[i] = true;
-            dfs(word, letters, used, result, &word_bank); // cannot pass something while im iterating on it, obviously
-            // word = word.substring(0, word.length() - 1)
-            // used[i] = false
+            dfs(word, letters, used, result, &word_bank);
+            word.pop();
+            used[i] = false;
         }
     }
 }
